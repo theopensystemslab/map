d95bfc6d9033ca2fbcb8f601278ca56544c994da
John Rees <john@bitsushi.com>
2021-07-13 16:09:21 +0100

    render map

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import { render } from "react-dom";
 import Component from "./Map";
 
+const viewport = {
+  latitude: 51,
+  longitude: 0,
+  zoom: 10,
+  bearing: 0,
+  pitch: 0,
+};
+
 render(
-  <Component LAYER_ORDNANCE_SURVEY="ordnance-survey" layer="ordnance-survey" />,
+  <Component
+    LAYER_ORDNANCE_SURVEY="ordnance-survey"
+    layer="ordnance-survey"
+    {...viewport}
+  />,
   document.getElementById("root")
 );
