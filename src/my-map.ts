664d385246fa819a328b2c0744e8cbbe148a3c44
John Rees <johnrees@users.noreply.github.com>
2021-07-30 11:39:10 +0100

    dispatch events (#13)
    * dispatch areaChange event
    
    * payload: any
    
    * console.debug
    
    * geojsonChange event

--- src/my-map.ts
+++ src/my-map.ts
@@ -1,10 +1,11 @@
 import { css, customElement, html, LitElement, property } from "lit-element";
 import stylefunction from "ol-mapbox-style/dist/stylefunction";
+import { GeoJSON } from "ol/format";
 import { Draw, Modify, Snap } from "ol/interaction";
 import Map from "ol/Map";
 import { fromLonLat, transformExtent } from "ol/proj";
 import View from "ol/View";
-
+import { last } from "rambda";
 import { drawingLayer, drawingSource, formatArea } from "./draw";
 import { osVectorTileBaseMap, rasterBaseMap } from "./os-layers";
 
@@ -16,12 +17,20 @@ export class MyMap extends LitElement {
       display: block;
       width: 800px;
       height: 800px;
+      position: relative;
     }
     #map {
       height: 100%;
       opacity: 0;
       transition: opacity 0.25s;
       overflow: hidden;
+    }
+    #area {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      z-index: 100;
+      background: white;
     }
   `;
 
@@ -43,9 +52,6 @@ export class MyMap extends LitElement {
 
   @property({ type: Boolean })
   drawMode = true;
-
-  @property({ type: String })
-  totalArea = "";
 
   private useVectorTiles =
     Boolean(import.meta.env.VITE_APP_ORDNANCE_SURVEY_KEY) &&
@@ -107,11 +113,17 @@ export class MyMap extends LitElement {
 
       // 'change' ensures getFeatures() isn't empty and listens for modifications; 'drawend' does not
       drawingSource.on("change", () => {
-        let sketches = drawingSource.getFeatures();
-        let last_sketch_geom =
-          sketches[sketches.length - 1]["values_"].geometry;
+        const sketches = drawingSource.getFeatures();
+        const lastSketchGeom = last(sketches).getGeometry();
 
-        this.totalArea = formatArea(last_sketch_geom);
+        this.dispatch(
+          "geojsonChange",
+          new GeoJSON().writeFeaturesObject(sketches, {
+            featureProjection: "EPSG:3857",
+          })
+        );
+
+        this.dispatch("areaChange", formatArea(lastSketchGeom));
       });
     }
 
@@ -119,6 +131,7 @@ export class MyMap extends LitElement {
     setTimeout(() => {
       window.dispatchEvent(new Event("resize"));
       target.style.opacity = "1";
+      this.dispatch("ready");
     }, 500);
   }
 
@@ -126,11 +139,20 @@ export class MyMap extends LitElement {
   render() {
     return html`<script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
       <link rel="stylesheet" href="https://cdn.skypack.dev/ol@^6.6.1/ol.css" />
-      <div id="map" />
-      ${this.drawMode
-        ? html`<span id="area">${this.totalArea}</span>`
-        : null} `;
+      <div id="map" />`;
   }
+
+  /**
+   * dispatches an event for clients to subscribe to
+   * @param eventName
+   * @param payload
+   */
+  private dispatch = (eventName: string, payload?: any) =>
+    this.dispatchEvent(
+      new CustomEvent(eventName, {
+        detail: payload,
+      })
+    );
 }
 
 declare global {
