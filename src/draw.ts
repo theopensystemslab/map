ae0f2ea76d66b5ac53d5225ec056e4c89f4fd154
Jessica McInchak <jessica.mcinchak@gmail.com>
2021-08-06 10:52:05 +0200

    formatArea as util, optional units param for hectares (#18)

--- src/draw.ts
+++ src/draw.ts
@@ -1,8 +1,6 @@
-import Geometry from "ol/geom/Geometry";
 import { Draw, Modify, Snap } from "ol/interaction";
 import { Vector as VectorLayer } from "ol/layer";
 import { Vector as VectorSource } from "ol/source";
-import { getArea } from "ol/sphere";
 import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
 
 const redLineStroke = new Stroke({
@@ -56,15 +54,3 @@ export const modify = new Modify({
     image: drawingPointer,
   }),
 });
-
-// calculate and format the area of a polygon
-export function formatArea(polygon: Geometry) {
-  const area = getArea(polygon);
-  let output;
-  if (area > 10000) {
-    output = Math.round((area / 1000000) * 100) / 100 + " " + "km<sup>2</sup>";
-  } else {
-    output = Math.round(area * 100) / 100 + " " + "m<sup>2</sup>";
-  }
-  return output;
-}
