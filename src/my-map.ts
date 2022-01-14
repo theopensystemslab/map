bc793a2da9b7885009b8ca0a4331a27cb15b8fe8
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-01-14 10:54:22 +0100

    tweak order of drawingSource.clear() too

--- src/my-map.ts
+++ src/my-map.ts
@@ -329,18 +329,19 @@ export class MyMap extends LitElement {
 
     // draw interactions
     if (this.drawMode) {
-      // check if single polygon feature was provided to load as the initial drawing
+      // make sure drawingSource is cleared upfront, even if drawGeojsonData is provided
+      drawingSource.clear();
+
+      // load an initial polygon into the drawing source if provided, or start from an empty drawing source
       const loadInitialDrawing =
         Object.keys(this.drawGeojsonData.geometry).length > 0;
+
       if (loadInitialDrawing) {
         let feature = new GeoJSON().readFeature(this.drawGeojsonData, {
           featureProjection: "EPSG:3857",
         });
         drawingSource.addFeature(feature);
-        // fit map to extent of intial feature, overriding zoom & lat/lng center
         fitToData(map, drawingSource, this.drawGeojsonDataBuffer);
-      } else {
-        drawingSource.clear();
       }
 
       map.addLayer(drawingLayer);
