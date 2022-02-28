e98a4f83575ae557fcd2c809a4dab1e9a99f27d2
John Rees <johnrees@users.noreply.github.com>
2022-02-28 12:51:19 -0500

    wip: multiple build entry files

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,24 @@ import litcss from 'rollup-plugin-postcss-lit';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: {
-      entry: "src/my-map.ts",
-      formats: ["es", "umd"],
-      name: "map",
-    },
     rollupOptions: {
-      // external: /^lit-element/,
-      // input: {
-      //   main: resolve(__dirname, "index.html"),
-      // },
+      input: {
+        "my-map": "src/my-map.ts",
+        "address-autocomplete": "src/components/address-autocomplete/index.ts",
+        "postcode-search": "src/components/postcode-search/index.ts",
+      },
+      output: [
+        {
+          entryFileNames: ({ name }) => `${name}.es.js`,
+          format: "es",
+          inlineDynamicImports: false,
+        },
+        {
+          entryFileNames: ({ name }) => `${name}.umd.js`,
+          format: "umd",
+          inlineDynamicImports: true,
+        },
+      ],
     },
   },
   plugins: [
