ebc2495928ef50e4b15887efcb6966390caf586b
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-08-04 12:13:31 +0200

    stub out map tests, outstanding type error

---
+++ src/components/my-map/main.test.ts
@@ -0,0 +1,33 @@
+import type { IWindow } from "happy-dom";
+import { afterEach, beforeEach, describe, it, expect, vi } from "vitest";
+
+import { getShadowRootEl } from "../../test-utils";
+
+// import "./index";
+
+declare global {
+  interface Window extends IWindow {}
+}
+
+describe.todo(
+  "PostcodeSearch on initial render with default props",
+  async () => {
+    // https://stackoverflow.com/questions/61683583/openlayers-6-typeerror-url-createobjecturl-is-not-a-function
+    global.URL.createObjectURL = vi.fn();
+
+    beforeEach(async () => {
+      document.body.innerHTML = '<my-map id="map-vitest" disableVectorTiles />';
+
+      await window.happyDOM.whenAsyncComplete();
+    }, 1000);
+
+    afterEach(async () => {
+      vi.resetAllMocks();
+    });
+
+    it("should be keyboard navigable", () => {
+      const input = getShadowRootEl("my-map", "div");
+      expect(input?.getAttribute("tabindex")).toEqual("0");
+    });
+  }
+);
