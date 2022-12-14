7b445e717a321a23e0d639ae65fe6b41657b8a32
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-12-14 08:13:12 +0000

    fix: Display static geojson in docs

--- src/components/my-map/docs/my-map-geojson.doc.js
+++ src/components/my-map/docs/my-map-geojson.doc.js
@@ -35,34 +35,32 @@ module.exports = {
       title: "Show a GeoJSON polygon on a static map",
       description:
         "Show a custom GeoJSON polygon on an OS basemap. Hide the zoom & reset control buttons.",
-      controller: function (element) {
-        const map = element.querySelector("my-map");
-        map.geojsonData = {
-          type: "Feature",
-          properties: {},
-          geometry: {
-            type: "Polygon",
-            coordinates: [
-              [
-                [-0.11562466621398924, 51.50746034612789],
-                [-0.11449813842773436, 51.50617151942102],
-                [-0.11359691619873047, 51.50653212745768],
-                [-0.11348962783813477, 51.50645199258505],
-                [-0.1127493381500244, 51.50665900738443],
-                [-0.11383295059204103, 51.507907754128546],
-                [-0.11562466621398924, 51.50746034612789],
-              ],
-            ],
-          },
-        };
-      },
       template: `
         <my-map
           geojsonColor="#ff0000"
           geojsonBuffer="10"
           hideResetControl
           staticMode 
-          osVectorTilesApiKey="" />`,
+          osVectorTilesApiKey="" 
+          geojsonData=${JSON.stringify({
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "Polygon",
+              coordinates: [
+                [
+                  [-0.11562466621398924, 51.50746034612789],
+                  [-0.11449813842773436, 51.50617151942102],
+                  [-0.11359691619873047, 51.50653212745768],
+                  [-0.11348962783813477, 51.50645199258505],
+                  [-0.1127493381500244, 51.50665900738443],
+                  [-0.11383295059204103, 51.507907754128546],
+                  [-0.11562466621398924, 51.50746034612789],
+                ],
+              ],
+            },
+          })}
+        />`,
     },
   ],
 };
