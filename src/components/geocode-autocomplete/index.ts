1012aca0e58e55b9bddab457de0a1ddbcf9d1459
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2025-09-12 09:18:43 +0100

    style: Geocode autocomplete (#577)

--- src/components/geocode-autocomplete/index.ts
+++ src/components/geocode-autocomplete/index.ts
@@ -15,7 +15,6 @@ type Address = {
   };
 };
 
-type ArrowStyleEnum = "default" | "light";
 type LabelStyleEnum = "responsive" | "static";
 
 function debounce(
@@ -59,9 +58,6 @@ export class GeocodeAutocomplete extends LitElement {
   osProxyEndpoint = "";
 
   @property({ type: String })
-  arrowStyle: ArrowStyleEnum = "default";
-
-  @property({ type: String })
   labelStyle: LabelStyleEnum = "responsive";
 
   // internal reactive state
@@ -87,10 +83,6 @@ export class GeocodeAutocomplete extends LitElement {
     super.disconnectedCallback();
   }
 
-  _getLightDropdownArrow() {
-    return '<svg class="autocomplete__dropdown-arrow-down" style="height: 17px;" viewBox="0 0 512 512" ><path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z"/></svg>';
-  }
-
   // called after the initial render
   firstUpdated() {
     // https://github.com/alphagov/accessible-autocomplete
@@ -112,8 +104,6 @@ export class GeocodeAutocomplete extends LitElement {
       showAllValues: true,
       displayMenu: "overlay",
       minLength: 3,
-      dropdownArrow:
-        this.arrowStyle === "light" ? this._getLightDropdownArrow : undefined,
       tNoResults: () => (this._isLoading ? `Loading...` : `No results found`),
       tStatusQueryTooShort: (minQueryLength: number) =>
         `Type at least ${minQueryLength} characters for search results`,
